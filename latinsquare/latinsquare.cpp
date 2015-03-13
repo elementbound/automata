5be9944724e906e0039429fa72cfd7fc75448668
Gálffy Tamás <ezittgtx@gmail.com>
2015-03-13 11:39:22 +0100

    Latin square algorithm done

--- latinsquare/latinsquare.cpp
+++ latinsquare/latinsquare.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm> //random_shuffle
 #include <vector>
+#include <utility> //std::pair
 
 class matrix
 {
@@ -21,53 +22,112 @@ class matrix
 		{
 			return m_Data[y*m_Width + x];
 		}
-}
+		
+		unsigned width() const { return m_Width; }
+		unsigned height() const { return m_Height; }
+};
+
+class latin_square
+{
+	private: 
+		matrix 					m_Matrix;
+		std::vector<unsigned> 	m_ValueSet;
+		
+		std::pair<bool, unsigned> check_row_collision(unsigned x, unsigned y, unsigned value)
+		{
+			for(unsigned cx = 0; cx < x; cx++)
+			{
+				if(m_Matrix(cx,y) == value)
+					return {true, cx};
+			}
+			
+			return {false, 0};
+		}
+		
+	public: 
+		void print()
+		{
+			for(unsigned y=0; y<m_Matrix.height(); y++)
+			{
+				for(unsigned x=0; x<m_Matrix.width(); x++)
+					std::cout << m_Matrix(x,y) << ' ';
+				std::cout << '\n';
+			}
+		}
+		
+		void generate(unsigned s)
+		{
+			m_Matrix.resize(s,s);
+			m_ValueSet.resize(s);
+			std::generate(m_ValueSet.begin(), m_ValueSet.end(), 
+				[]() -> unsigned {
+					static unsigned i=0;
+					return i++;
+				});
+				
+			for(unsigned column = 0; column < s; column++)
+			{
+				//Generate a random column
+				std::random_shuffle(m_ValueSet.begin(), m_ValueSet.end());
+				
+				//Copy into the matrix
+				for(unsigned row = 0; row < s; row++)
+					m_Matrix(column, row) = m_ValueSet[row];
+				
+				//Check for collisions, if necessary 
+				if(column == 0)
+					continue;
+				
+				for(unsigned row=0; row<s; row++)
+				{
+					std::pair<bool, unsigned> collision = check_row_collision(column, row, m_Matrix(column, row));
+					if(!collision.first) continue; //No collision 
+					bool collision_resolved = 0;
+					
+					//Found a collision, now find a row where we can swap the last values 
+					//to resolve the collision
+					for(unsigned swap_row=0; swap_row<s; swap_row++)
+					{
+						if(check_row_collision(column, row, m_Matrix(column, swap_row)).first)
+							continue; 
+						
+						if(check_row_collision(column, swap_row, m_Matrix(column, row)).first)
+							continue;
+						
+						//Found a swap, great! 
+						std::swap(m_Matrix(column, swap_row), m_Matrix(column, row));
+						collision_resolved = 1;
+						break;
+					}
+					
+					//If no swap was found, fall back to permutations 
+					while(!collision_resolved)
+					{
+						std::next_permutation(m_ValueSet.begin(), m_ValueSet.end());
+						for(unsigned row=0; row<s; row++)
+							if(!check_row_collision(column, row, m_ValueSet[row]).first)
+							{
+								collision_resolved = 1;
+								break;
+							}
+							
+						//Copy into column
+						if(collision_resolved) 
+							for(unsigned row = 0; row < s; row++)
+								m_Matrix(column, row) = m_ValueSet[row];
+					}
+				}
+			}
+		}
+};
 
 int main()
 {
-	unsigned size = 8;
+	unsigned size = 10;
 	
-	std::vector<unsigned> valset;
-	matrix square;
+	latin_square sqr;
+	sqr.generate(size);
+	sqr.print();
 	
-	valset.resize(size);
-	for(unsigned i=0; i<size; i++)
-		valset[i] = i;
-	
-	matrix.resize(size,size);
-	
-	for(unsigned x=0; x<size; x++)
-	{
-		std::random_shuffle(valset);
-			
-		for(unsigned y=0; y<size; y++)
-			square(x,y) = valset[y];
-		
-		if(x==0)
-			continue; 
-		
-		bool collision = 0;
-		for(unsigned y=0; y<size; y++)
-			for(unsigned cx=x-1; cx<x; x++)
-				if(square(cx,y) == square(x,y))
-				{
-					collision = 1;
-					break;
-				}
-				
-		if(!collision)
-			continue;
-		
-		for(unsigned sy=0; sy<size; sy++)
-		{
-			if(sy == y)
-				continue;
-			
-			collision = 0;
-			for(unsigned cx=x-1; x<x; x++)
-			{
-				
-			}
-		}
-	}
+	return 0;
 }

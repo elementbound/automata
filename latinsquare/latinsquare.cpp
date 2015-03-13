3202c3eb124abd574cc4bb1f6d3cd747a86c11a5
Gálffy Tamás <ezittgtx@gmail.com>
2015-03-13 14:02:20 +0100

    Added a simple test to latinsquare.cpp::main

--- latinsquare/latinsquare.cpp
+++ latinsquare/latinsquare.cpp
@@ -97,21 +97,63 @@ class latin_square
 				for(unsigned x=0; x<s; x++)
 					m_Matrix(x,y) = m_ValueSet[m_Matrix(x,y)];
 		}
+		
+		matrix& operator()(){return m_Matrix;}
 };
 
 int main(int argc, char** argv)
 {
 	unsigned size = (argc>1) ? atoi(argv[1]) : 10;
 	unsigned seed = (argc>2) ? atoi(argv[2]) : std::chrono::system_clock::now().time_since_epoch().count();
-	unsigned test_count = 8;
+	unsigned test_count = (argc>3) ? atoi(argv[3]) : 8192;
+	unsigned test_success = test_count;
 	
 	std::mt19937 rng;
 	rng.seed(seed);
-	
-	std::cout << "A sample latinsquare(" << size << ")" << std::endl;
 	latin_square sqr;
-	sqr.generate(size, rng);
-	sqr.print();
+
+	if(size < 32)
+	{
+		std::cout << "A sample latinsquare(" << size << ", " << seed << ")" << std::endl;
+		sqr.generate(size, rng);
+		sqr.print();
+	}
+	
+	for(unsigned test_id = 0; test_id < test_count; test_id++)
+	{
+		std::cout << "Doing test " << (test_id+1) << "/" << test_count << '\r';
+		sqr.generate(size, rng);
+		
+		for(unsigned check_y=0; check_y<size; check_y++)
+		{
+			for(unsigned check_x=0; check_x<size; check_x++)
+			{
+				//Check row
+				for(unsigned x=0; x<size; x++)
+					if(sqr()(x,check_y) == sqr()(check_x,check_y) && x!=check_x)
+					{
+						std::cout << "\nTest fail!" << std::endl;
+						sqr.print();
+						--test_success;
+						break; //from check row
+						break; break; //from each-cell
+					}
+					
+				//Check column
+				for(unsigned y=0; y<size; y++)
+					if(sqr()(check_x,y) == sqr()(check_x,check_y) && y!=check_y)
+					{
+						std::cout << "\nTest fail!" << std::endl;
+						sqr.print();
+						--test_success;
+						break; //from check column
+						break; break; //from each-cell
+					}
+			}
+		}
+	}
+	
+	std::cout << std::endl << test_success << "/" << test_count << " passed" << std::endl;
 	
 	return 0;
 }

84cf25fc1ca2e1308d81f6d4dd4be46461054da3
Gálffy Tamás <ezittgtx@gmail.com>
2015-03-17 16:18:04 +0100

    Some additional cleanup

--- latinsquare/grid.h
+++ latinsquare/grid.h
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <iostream>
+#include <random>
+#include <algorithm>
 
 class grid
 {
@@ -37,6 +39,51 @@ class grid
 		const_reverse_iterator crbegin() const; 
 		const_reverse_iterator crend() const;
 };
+
+//
+
+template <typename RNG> 
+grid generate_latin_square(unsigned s, RNG rng){
+	grid retgrid;
+	std::vector<unsigned> value_set;
+	
+	retgrid.resize(s,s);
+	value_set.resize(s);
+	std::uniform_int_distribution<unsigned> udr(0, s-1);
+	
+	std::generate(value_set.begin(), value_set.end(), 
+		[]() -> unsigned {
+			static unsigned i=0;
+			return i++;
+		});
+	
+	for(unsigned y=0; y<s; y++)
+		for(unsigned x=0; x<s; x++)
+			retgrid(x,y) = (x+y)%s;
+		
+	//Shuffle rows 
+	for(unsigned y=s-1; y>0; y--)
+	{
+		unsigned swap_row = udr(rng) % y;
+		for(unsigned x=0; x<s; x++)
+			std::swap(retgrid(x,y), retgrid(x,swap_row));
+	}
+	
+	//Randomly remap values
+	std::shuffle(value_set.begin(), value_set.end(), rng);
+	std::transform(retgrid.begin(), retgrid.end(), retgrid.begin(), 
+		[&, value_set] (unsigned i) -> unsigned {
+			return value_set[i];
+		}
+	);
+	
+	return retgrid;
+}
+
+inline grid generate_latin_square(unsigned s)
+{
+	return generate_latin_square(s, std::default_random_engine());
+}
 
 std::ostream& operator<<(std::ostream& os, const grid& g);
 
--- latinsquare/latinsquare.cpp
+++ latinsquare/latinsquare.cpp
@@ -1,56 +1,9 @@
 #include <iostream>
-#include <algorithm> //std::shuffle, std::swap
-#include <vector>
-#include <utility> //std::pair
 #include <random>
 #include <chrono> //std::system_clock::now
 #include <cstdlib> //atoi
 
 #include "grid.h"
-
-template <typename RNG> 
-grid generate_latin_square(unsigned s, RNG rng)
-{
-	grid retgrid;
-	std::vector<unsigned> value_set;
-	
-	retgrid.resize(s,s);
-	value_set.resize(s);
-	std::uniform_int_distribution<unsigned> udr(0, s-1);
-	
-	std::generate(value_set.begin(), value_set.end(), 
-		[]() -> unsigned {
-			static unsigned i=0;
-			return i++;
-		});
-	
-	for(unsigned y=0; y<s; y++)
-		for(unsigned x=0; x<s; x++)
-			retgrid(x,y) = (x+y)%s;
-		
-	//Shuffle rows 
-	for(unsigned y=s-1; y>0; y--)
-	{
-		unsigned swap_row = udr(rng) % y;
-		for(unsigned x=0; x<s; x++)
-			std::swap(retgrid(x,y), retgrid(x,swap_row));
-	}
-	
-	//Randomly remap values
-	std::shuffle(value_set.begin(), value_set.end(), rng);
-	std::transform(retgrid.begin(), retgrid.end(), retgrid.begin(), 
-		[&, value_set] (unsigned i) -> unsigned {
-			return value_set[i];
-		}
-	);
-	
-	return retgrid;
-}
-
-grid generate_latin_square(unsigned s)
-{
-	return generate_latin_square(s, std::default_random_engine());
-}
 
 int main(int argc, char** argv)
 {

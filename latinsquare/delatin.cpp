1307a6800aad356a63d17dd99085092ac89b33c8
Gálffy Tamás <ezittgtx@gmail.com>
2015-03-17 16:53:27 +0100

    Rewritten enlatin's and delatin's file reading loop. Didn't help.

--- latinsquare/delatin.cpp
+++ latinsquare/delatin.cpp
@@ -19,26 +19,34 @@ int main(int argc, char** argv)
 	automata_outputs 	 = generate_latin_square(256, rng);
 	DEBUG("done\n");
 	
+	const unsigned buffer_size = 8192;
+	unsigned char* buffer = new unsigned char[buffer_size];
+	
 	while(std::cin.good())
 	{
-		int c = 0;
-		c = std::cin.get();
-		
-		//Do a reverse lookup
-		for(unsigned y=0; y<256; y++)
+		std::cin.read((char*)buffer, buffer_size);
+		for(unsigned i=0; i<std::cin.gcount(); i++)
 		{
-			if(automata_outputs(automata_state, y) == c)
-			{
-				std::cout << (char)y;
-				automata_state = automata_transitions(automata_state, y);
-				
-				break;
-			}
+			int c = buffer[i];
 			
-			if(y == 255)
-				std::cerr << "No matching character found for " << c << "!\n";
+			//Do a reverse lookup
+			for(unsigned y=0; y<256; y++)
+			{
+				if(automata_outputs(automata_state, y) == c)
+				{
+					std::cout << (char)y;
+					automata_state = automata_transitions(automata_state, y);
+					
+					break;
+				}
+				
+				if(y == 255)
+					std::cerr << "No matching character found for " << c << "!\n";
+			}
 		}
 	}
+	
+	delete [] buffer;
 	
 	return 0;
 }
--- latinsquare/enlatin.cpp
+++ latinsquare/enlatin.cpp
@@ -14,17 +14,22 @@ int main(int argc, char** argv)
 	automata_transitions = generate_latin_square(256, rng);
 	automata_outputs 	 = generate_latin_square(256, rng);
 	
-	while(1)
+	const unsigned buffer_size = 8192;
+	unsigned char* buffer = new unsigned char[buffer_size];
+	
+	while(std::cin.good())
 	{
-		int c = 0;
-		c = std::cin.get();
-		
-		if( c == EOF) 
-			break;
-		
-		std::cout << (char)automata_outputs(automata_state, c);
-		automata_state = automata_transitions(automata_state, c);
+		std::cin.read((char*)buffer, buffer_size);
+		for(unsigned i=0; i<std::cin.gcount(); i++)
+		{
+			unsigned char c = buffer[i];
+			
+			std::cout << (char)automata_outputs(automata_state, c);
+			automata_state = automata_transitions(automata_state, c);
+		}
 	}
+	
+	delete [] buffer;
 	
 	return 0;
 }

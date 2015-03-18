bc6d11b36c3a3bab40eefa62c133fcbd3a270a8b
Gálffy Tamás <ezittgtx@gmail.com>
2015-03-18 13:56:14 +0100

    Delatin now precomputes a reverse lookup table instead doing it per-character

--- latinsquare/delatin.cpp
+++ latinsquare/delatin.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv)
 	const char* output_file = argv[3];
 	grid automata_transitions;
 	grid automata_outputs;
+	grid reverse_lookup_table;
 	unsigned automata_state;
 	std::mt19937 rng;
 	
@@ -23,6 +24,18 @@ int main(int argc, char** argv)
 	automata_transitions = generate_latin_square(256, rng);
 	automata_outputs 	 = generate_latin_square(256, rng);
 	automata_state = rng() % 256;
+	
+	reverse_lookup_table.resize(256, 256);
+	for(unsigned x=0; x<256; x++)
+	{
+		for(unsigned y=0; y<256; y++)
+			for(unsigned cy=0; cy<256; cy++)
+				if(automata_outputs(x,cy) == y)
+				{
+					reverse_lookup_table(x,y) = cy;
+					break;
+				}
+	}
 	
 	const unsigned buffer_size = 8192;
 	unsigned char* buffer = new unsigned char[buffer_size];
@@ -42,18 +55,9 @@ int main(int argc, char** argv)
 		for(unsigned i=0; i<fis.gcount(); i++)
 		{
 			int c = buffer[i];
-			
-			//Do a reverse lookup
-			for(unsigned y=0; y<256; y++)
-			{
-				if(automata_outputs(automata_state, y) == c)
-				{
-					fos << (char)y;
-					automata_state = automata_transitions(automata_state, y);
-					
-					break;
-				}
-			}
+			int d = reverse_lookup_table(automata_state, c);
+			fos << (char)d;
+			automata_state = automata_transitions(automata_state, d);
 		}
 	}
 	

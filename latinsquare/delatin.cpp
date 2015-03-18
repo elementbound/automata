42ee0c116b7d59e2f31671e523ad6331d1b7c81c
Gálffy Tamás <ezittgtx@gmail.com>
2015-03-18 13:40:54 +0100

    You can now set en/delatin's seed as a cmdline parameter

--- latinsquare/delatin.cpp
+++ latinsquare/delatin.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <random>
+#include <cstdlib> //atoi
 #include "grid.h"
 
 #define die(msg) {std::cerr << msg << '\n'; return 1;}
 
 int main(int argc, char** argv)
 {
-	if(argc < 2)
-		die("Usage: " << argv[0] << " <input file> <output file>");
+	if(argc < 3)
+		die("Usage: " << argv[0] << " <seed> <input file> <output file>");
 	
-	unsigned seed = 8;
+	unsigned seed = atoi(argv[1]);
+	const char* input_file = argv[2];
+	const char* output_file = argv[3];
 	grid automata_transitions;
 	grid automata_outputs;
-	unsigned automata_state = 0;
+	unsigned automata_state;
 	std::mt19937 rng;
 	
 	rng.seed(seed);
 	automata_transitions = generate_latin_square(256, rng);
 	automata_outputs 	 = generate_latin_square(256, rng);
+	automata_state = rng() % 256;
 	
 	const unsigned buffer_size = 8192;
 	unsigned char* buffer = new unsigned char[buffer_size];
 	
-	std::ifstream fis(argv[1], std::ios::binary);
-	std::ofstream fos(argv[2], std::ios::binary | std::ios::trunc);
+	std::ifstream fis(input_file, std::ios::binary);
+	std::ofstream fos(output_file, std::ios::binary | std::ios::trunc);
 	
 	if(!fis.good())
-		die("Couldn't open input file: " << argv[1]);
+		die("Couldn't open input file: " << input_file);
 	
 	if(!fos.good())
-		die("Couldn't open output file: " << argv[2]);
+		die("Couldn't open output file: " << output_file);
 	
 	while(fis.good() && fos.good())
 	{

d567cbd5d54d26558b5ed7061bb9f22b6a50783a
Gálffy Tamás <ezittgtx@gmail.com>
2015-04-13 21:46:24 +0200

    Added bitstream again. So I can use varying-length outputs.

---
+++ domosi/bitstream.cpp
@@ -0,0 +1,84 @@
+#include "bitstream.h"
+
+//=============================================================================================
+//ibitstream
+
+bool ibitstream::get() {
+	if(m_ReserveCount > 0) {
+		bool ret = m_ReserveData & 0x1;
+		m_ReserveData >>= 1;
+		m_ReserveCount--;
+		
+		return ret;
+	}
+	else 
+	{
+		m_ReserveData = m_InputStream->get();
+		m_ReserveCount = 8;
+		
+		return this->get();
+	}
+}
+
+void ibitstream::get(char* data, size_t bit_count) {
+	for(size_t i=0; i<bit_count; i++) {
+		size_t byte_id = i/8;
+		size_t bit_id = i%8;
+		
+		if(bit_id == 0)
+			data[byte_id] = 0;
+		
+		data[byte_id] |= this->get() << bit_id;
+	}
+}
+
+void ibitstream::istream(std::istream* is) {
+	m_InputStream = is;
+}
+
+std::istream& ibitstream::istream() {
+	return *m_InputStream;
+}
+
+const std::istream& ibitstream::istream() const {
+	return *m_InputStream;
+}
+
+//=============================================================================================
+//obitstream
+
+void obitstream::put(bool v) {
+	m_ReserveData = (m_ReserveData | (v<<7));
+	m_ReserveCount++;
+	
+	if(m_ReserveCount == 8)
+		this->flush();
+	else 
+		m_ReserveData >>= 1;
+}
+
+void obitstream::put(const char* data, size_t bit_count) {
+	for(size_t i=0; i<bit_count; i++) {
+		size_t byte_id = i/8;
+		size_t bit_id = i%8;
+		
+		this->put(data[byte_id] & (1 << bit_id));
+	}
+}
+
+void obitstream::flush() {
+	m_OutputStream.put(m_ReserveData);
+	m_ReserveCount = 0;
+}
+
+void obitstream::ostream(std::ostream* os) {
+	m_OutputStream = os;
+}
+
+std::ostream& obitstream::ostream() {
+	return m_OutputStream;
+}
+
+const std::ostream& obitstream::ostream() const {
+	return m_OutputStream;
+}
---
+++ domosi/bitstream.h
@@ -0,0 +1,43 @@
+#ifndef _H_BITSTREAM_H_
+#define _H_BITSTREAM_H_
+
+#include <iostream>
+#include <cstdint> //uint8_t
+
+class ibitstream
+{
+	private: 
+		std::istream* m_InputStream;
+		//Reserved bits storage and number of bits
+		uint8_t m_ReserveData = 0;
+		uint8_t m_ReserveCount = 0;
+		
+	public: 
+		bool get();
+		void get(char* data, size_t bit_count);
+		
+		void 				istream(std::istream*);
+		std::istream& 		istream();
+		const std::istream& istream() const;
+};
+
+class obitstream
+{
+	private: 
+		std::ostream* m_OutputStream;
+		//Reserved bits storage and number of bits
+		uint8_t m_ReserveData = 0;
+		uint8_t m_ReserveCount = 0;
+		
+	public: 
+		void put(bool);
+		void put(const char* data, size_t bit_count);
+		
+		void flush();
+		
+		void 				ostream(std::ostream*);
+		std::ostream& 		ostream();
+		const std::ostream& ostream() const;
+}
+
+#endif
